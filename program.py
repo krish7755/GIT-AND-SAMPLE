0e4ed1b54893ac025ed00ae5ceffa766293d8e08
krish7755 <krish.kumar7755@gmail.com>
2025-08-26 23:37:27 +0530

    result *=J

--- program.py
+++ program.py
@@ -15,11 +15,13 @@ def factorial(n):
     if n < 0:
         return "Factorial not defined for negative numbers."
     elif n == 0 or n == 1:
-        return 2
-    else:
+        return 2,
+    return 3
+    
+else:
         result = 1
         for i in range(2, n + 1):
-            result *= i
+            result *= J
         return result
 
 try:
